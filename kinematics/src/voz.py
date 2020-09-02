a01316b789b2a3f6178fe41c33a8396a4879c2d4
noerato14 <38055100+noerato14@users.noreply.github.com>
2020-09-02 16:39:13 -0500

    Add files via upload

--- kinematics/src/voz.py
+++ kinematics/src/voz.py
@@ -1,28 +1,30 @@
-
 #!/usr/bin/env python
-from __future__ import division
 import time
 import rospy
-from playsound import playsound
+import pygame
 from node_mssg.msg import ViewTemplate
 
 
 def callback(data1):
 	caso = data1.caso
-
+	pygame.mixer.init()
 	print(caso)
 
 	if caso == '5': #borrar poner solo letra
-		playsound('~/catkin_ws/src/ARCA/kinematics/src/Intro_Op2.mp3')	
+		pygame.mixer.music.load("Intro_Op2.mp3")
+		#playsound('~/catkin_ws/src/ARCA/kinematics/src/Intro_Op2.mp3')	
 
 	if caso == '7':
-		playsound('~/catkin_ws/src/ARCA/kinematics/src/Despedida_Op2.mp3')	
+		pygame.mixer.music.load("Despedida_Op2.mp3")
+		#playsound('~/catkin_ws/src/ARCA/kinematics/src Despedida_Op2.mp3')	
 
 	if caso == '2':
-		playsound('~/catkin_ws/src/ARCA/kinematics/src/Saludo_Op2.mp3')
+		pygame.mixer.music.load("Saludo_Op2.mp3")
+		#playsound('~/catkin_ws/src/ARCA/kinematics/src/Saludo_Op2.mp3')
 
-
-
+	pygame.mixer.music.play()
+	while pygame.mixer.music.get_busy() == True:
+		continue
     
     
 
